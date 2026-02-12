9ab3f7fafff35a69f22b5dd430506bbd1e6c4d04
farima4 <jovozivanovic1995@gmail.com>
2026-02-12 23:24:42 +0100

    getNovels() function which refreshes contents every 5 minutes

--- main.go
+++ main.go
@@ -3,23 +3,49 @@ package main
 import (
     "fmt"
     "html/template"      // HTML templating
-//    "io/fs"             // File system interface
+    "io/fs"             // File system interface
     "net/http"          // HTTP server and client
 //    "os"                // File operations
 //    "path/filepath"     // Path manipulation
 //    "strings"           // String utilities
 	"log"
+	"sync"
+	"time"
 //    "github.com/gomarkdown/markdown"            // Main markdown package
 //    "github.com/gomarkdown/markdown/html"       // HTML renderer
 //    "github.com/gomarkdown/markdown/parser"     // Markdown parser
-)
+);
 
+// GLOBAL VARIABLES
 var (
-	port = "4040"
-)
+	port = "4040";
+	novels []Novel;
+	mutex sync.RWMutex;
+	lastScan time.Time;
+);
 
+//CUSTOM TYPES
+type Novel struct{
+	ID string;
+	Title string;
+	Description string;
+	Cover string;
+	Chapters []Chapter;
+};
+
+type Chapter struct{
+	ID string;
+	Title string;
+	Content template.HTML;
+	Slug string;
+};
+
+// FUNCTIONS
 func main(){
-	fmt.Println("Starting the webserver");
+	fmt.Println("Starting the webserver at port :" + port);
+
+	novels = scanNovels();
+	lastScan = time.Now();
 
 	http.HandleFunc("/", homePageHandler);
 
@@ -34,10 +60,26 @@ func homePageHandler(response http.ResponseWriter, request *http.Request){
 		return;
 	}
 
-	err = tmpl.Execute(response, nil)
+	err = tmpl.Execute(response, nil);
 	if err != nil { //desio se error
 		http.Error(response, err.Error(), http.StatusInternalServerError);
 	}
 }
 
+func getNovels []Novel(){
+	mutex.RLock();
+	if time.Since(lastScan) < 5*time.Minute {
+		defer mutex.RUnlock
+		return novels;
+	}
+	mutex.RUnlock();
 
+	mutex.Lock();
+	defer mutex.Unlock();
+	novels = scanNovels();
+	lastScan = time.Now();
+}
+
+func scanNovels []Novel{
+	
+}
